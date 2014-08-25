f3b35870a233e990dbdc656a892b1c5941c2a77c
Nitin Tyagi <ntyagi2@sapient.com>
2014-08-25 09:57:33 -0400

    Finalized the animate function with New Vector Map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){ 
 $(".hit").click(function(){
 	var path = $(this).parent('g').attr('relPath');
-    $.getJSON("js/data.json",function(result){
+
+    $.getJSON("js/data/data.json",function(result){
         var pathObj = result;
         $("#"+path).lazylinepainter({
 			"svgData": pathObj,
@@ -12,4 +13,34 @@ $(".hit").click(function(){
 		}).lazylinepainter('paint');
 	});
 });
+
 });
+
+/*$(document).ready(function(){
+  //Function which simulates zoom on the map on pinchin/pinchout
+  $('#map').hammer()
+    .on("pinchin", function(e) {
+      var scale = $(this).css('transform');
+      scale = (scale == null ? $(this).css('-webkit-transform') : scale); 
+      scale = (scale == null ? $(this).css('-ms-transform') : scale);
+      scale = scale.split(" ");
+      scale = parseFloat(scale[0].substring(7, scale[0].length - 1));
+
+      if(scale > 1) {
+        scale = ('scale(' + (scale - .1).toString() + ')');
+        $(this).css({'transform':scale, '-ms-transform':scale, '-webkit-transform':scale });
+      }
+    })
+    .on("pinchout", function(e) {
+      var scale = $(this).css('transform');
+      scale = (scale == null ? $(this).css('-webkit-transform') : scale); 
+      scale = (scale == null ? $(this).css('-ms-transform') : scale);
+      scale = scale.split(" ");
+      scale = parseFloat(scale[0].substring(7, scale[0].length - 1));
+
+      if(scale < 5) {
+        scale = ('scale(' + (scale + .1).toString() + ')');
+        $(this).css({'transform':scale, '-ms-transform':scale, '-webkit-transform':scale});
+      }
+  });
+});*/
